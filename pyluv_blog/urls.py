580b65ade6ca88a617839111f5db0000ad7c4bf5
agustinustheo <agustinustheoo@gmail.com>
2019-01-28 02:20:57 +0700

    added single page blogs to pyluv_blog app

--- pyluv_blog/urls.py
+++ pyluv_blog/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
 from . import views
+from django.urls import path
+from django.conf.urls import url, include
+from django.views.generic import ListView, DetailView
+from pyluv_blog.models import Post
 
 urlpatterns = [
-    path('', views.home, name="pyluv-blog-home"),
+    url('posts', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25],
+                            template_name="pyluv_blog/blog.html")),
+    path('home', views.home, name="pyluv-blog-home"),
+    url(r'^post/(?P<pk>\d+)$', DetailView.as_view(model = Post,
+                            template_name="pyluv_blog/post.html")),
 ]
